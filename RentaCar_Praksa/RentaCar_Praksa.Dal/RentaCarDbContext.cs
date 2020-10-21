69017f8d01d08a7a141206d4bd718e78ac6fabe8
Terzich <60022726+Terzich@users.noreply.github.com>
2020-10-21 09:36:04 +0200

    Adding Db Context
    Adding EF Core packages, DbSets ...

--- RentaCar_Praksa/RentaCar_Praksa.Dal/RentaCarDbContext.cs
+++ RentaCar_Praksa/RentaCar_Praksa.Dal/RentaCarDbContext.cs
@@ -1,13 +1,25 @@
-﻿using RentaCar_Praksa.Dal.Domain;
+﻿using Microsoft.EntityFrameworkCore;
+using RentaCar_Praksa.Dal.Domain;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace RentaCar_Praksa.Dal
 {
-    public class RentaCarDbContext
+    public class RentaCarDbContext:DbContext
     {
-        public List<User> Users { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<BookedCar> BookedCars { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+
+
+
 
     }
 }
